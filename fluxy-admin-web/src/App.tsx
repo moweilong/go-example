63f70edbb5acd99779fb9034e4dec01750cded7c
bugsmo <18550039021@163.com>
2024-10-14 22:53:38 +0800

    fluxy-admin-web: 登录页面样式修复

--- fluxy-admin-web/src/App.tsx
+++ fluxy-admin-web/src/App.tsx
@@ -1,16 +1,18 @@
 import { ConfigProvider, ThemeConfig, theme } from 'antd';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { useGlobalStore } from '@/store/global';
-import { routeConfig } from './config/routes';
+import { routeConfig } from '@/config/routes';
 
 import BasicLayout from '@/layouts';
-import Result404 from './404';
-import Login from './pages/user/login';
+import Result404 from '@/404';
+import Login from '@/pages/user/login';
+import { i18n } from '@/utils/i18n';
 
 function App() {
-  const darkMode = useGlobalStore(state => state.darkMode)
+
+  const { darkMode, lang } = useGlobalStore()
 
   const router = createBrowserRouter(
     [
@@ -30,18 +32,37 @@ function App() {
     ]
   );
 
-  const curTheme: ThemeConfig = useMemo(() => {
+  useEffect(() => {
     if (darkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, []);
+
+  const curTheme: ThemeConfig = useMemo(() => {
+
+    if (darkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+
       return {
         token: {
           colorPrimary: 'rgb(103, 58, 183)',
           // colorBgTextHover: '#f0e9f7',
-          // colorBgBase: 'rgb(17, 25, 54)',
+          colorBgBase: 'rgb(17, 25, 54)',
           // colorBgLayout: 'rgb(17, 25, 54)',
           colorBgContainer: 'rgb(26, 34, 63)',
           colorBorder: 'rgba(189, 200, 240, 0.157)',
           colorBgTextHover: 'rgba(124, 77, 255, 0.082)',
           colorTextHover: 'rgba(124, 77, 255, 0.082)',
+          controlItemBgActive: 'rgba(33, 150, 243, 0.16)',
         },
         algorithm: theme.darkAlgorithm,
       }
@@ -61,8 +82,12 @@ function App() {
   }, [darkMode])
 
   return (
-    <ConfigProvider theme={curTheme}>
-      <RouterProvider router={router} />
+    <ConfigProvider
+      theme={curTheme}
+    >
+      <RouterProvider
+        router={router}
+      />
     </ConfigProvider>
   );
 }
